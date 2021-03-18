dda565a12a5525a0bde3a8c9adc1bbbf77ac61d7
martin <labram03@gmail.com>
2021-03-18 19:14:14 -0400

    created models: Pig, Event, Reminder and Goal

--- piggybankapp/models.py
+++ piggybankapp/models.py
@@ -10,6 +10,49 @@ class User(models.Model):
     def __str__(self):
         return f"name: {self.name} , address: {self.address}"
 
+class Pig(models.Model):
+    pigId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    balance = models.IntegerField()
+    creationDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"name: {self.name} , balance: {self.balance}, status: {self.status}"
+
+
+class Event(models.Model):
+    eventId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    summary = models.CharField(max_length=100)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"name: {self.name} , date: {self.date}"
+
+class Reminder(models.Model):
+    reminderId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    date = models.DateTimeField()
+    type = models.CharField(max_length=50)
+    medium = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"name: {self.name} , date: {self.date}, medium: {self.medium}"
+
+
+class Goal(models.Model):
+    userId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    startingBalance = models.IntegerField(default=0)
+    endingBalance = models.IntegerField()
+    currentBalance = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return f"name: {self.name} , status: {self.status}, balance :{self.currentBalance}"
+
 
 class Profile(models.Model):
     profileId = models.AutoField(primary_key=True)
